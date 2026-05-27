5afabe352f694b9f4c0aea36672209cf32b90a77
Nitzan Zacharia <zacharia1@mail.tau.ac.il>
2026-05-27 10:57:00 +0300

    my files path

--- data_processing/process_json.py
+++ data_processing/process_json.py
@@ -5,9 +5,9 @@ import os
 # ==========================================
 # CENTRAL CONFIGURATION
 # ==========================================
-file_path_1 = r'C:\year3\recommenderSystem\beeradvocate.json\beeradvocate.json'
-file_path_2 = r'C:\year3\recommenderSystem\beeradvocate.json\ratebeer.json\ratebeer.json'
-log_file_path = r'C:\year3\recommenderSystem\bad_rows_log.txt'
+file_path_1 = r'C:\Users\nitza\OneDrive\Documents\RecSys\beeradvocate.json'
+file_path_2 = r'C:\Users\nitza\OneDrive\Documents\RecSys\ratebeer.json'
+log_file_path = r'C:\Users\nitza\OneDrive\Documents\RecSys\bad_rows_log.txt'
 
 DB_PARAMS = {
     "dbname": "recommend_db",
